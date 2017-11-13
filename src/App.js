e5339617917e24a7655a33b5713565547e4071d6
Austin Kabiru <austin.kabiru@andela.com>
2017-11-14 00:09:21 +0300

    define app as presentational component

--- src/App.js
+++ src/App.js
@@ -6,35 +6,6 @@ import Thread from './components/Thread'
 import Tabs from './components/Tabs'
 
 const store = createStore(reducer);
-
-class App extends React.Component {
-  componentDidMount() {
-    store.subscribe(() => this.forceUpdate());
-  }
-
-  render() {
-    const state = store.getState();
-    const activeThreadId = state.activeThreadId;
-    const threads = state.threads;
-    const activeThread = threads.find((t) => t.id === activeThreadId);
-
-    const tabs = threads.map(t => (
-      {
-        title: t.title,
-        active: t.id === activeThreadId,
-        id: t.id,
-      }
-    ));
-
-    return (
-      <div className='ui segment'>
-        <ThreadTabs tabs={tabs} />
-        <ThreadDisplay />
-      </div>
-    );
-  }
-}
-
 class ThreadTabs extends React.Component {
   componentDidMount() {
     store.subscribe(() => this.forceUpdate());
@@ -97,5 +68,12 @@ class ThreadDisplay extends React.Component {
     );
   }
 }
+
+const App = () => (
+  <div className="ui segment">
+    <ThreadTabs />
+    <ThreadDisplay />
+  </div>
+)
 
 export default App;
